5666efccf962a62f64c29d35fcff09cb474b1c47
Arturo <arturogabriel25@gmail.com>
2022-04-04 12:05:43 -0400

    haciendo pruebas

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 
-const submit_btn = document.querySelector("#submit")
-const rating_btn = document.querySelectorAll(".botones")
-
-
+const submit_btn = document.querySelector("#submit") // Selecciona boton submit
+const rating_btn = document.querySelectorAll(".botones") //Selecciona botones rating
+const card_1 = document.querySelector("#container1") // Seleccioan el card 1
+const card_2 = document.querySelector("#container2") // Selecciona el card 2 
+const score = document.querySelector("#score") // Selecciona el Span con el score
+let stars_score = 3 // valor por defecto
 
 
 const onsubmit = () => {
-    console.log("te toque");
+   
+    card_2.style.display = "flex"
+    card_1.style.display = "none"
+    score.textContent = stars_score // renderiza el valor del boton
 }
 
 const handleClick = (e) => {
-   
+    stars_score = e.target.value // Almacena el valor del boton
 }
+
+
 
 submit_btn.addEventListener("click", onsubmit) // Listener al submit
 
